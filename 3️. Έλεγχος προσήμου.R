14653bb07b82dfbecd65429a25800bf74ab0b18f
giotavoul <panavoul4@agro.duth.gr>
2025-03-22 22:25:53 +0200

    3. Έλεγχος προσήμου

---
+++ 3️. Έλεγχος προσήμου.R
@@ -0,0 +1,12 @@
+# 3️. Έλεγχος προσήμου
+theNumls=function(num){
+     if (num > 0) {print("positive")}
+     if (num < 0) {print("negative")}
+     if (num == 0) {print("zero")}
+}
+# > theNumls(-5)
+# [1] "negative"
+# > theNumls(2)
+# [1] "positive"
+# > theNumls(0)
+# [1] "zero"
